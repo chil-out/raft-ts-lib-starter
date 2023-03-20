e9789380243a388f3a31081bef50785e926ba2ec
rocket-x <zcjb.rjdj@gmail.com>
2023-03-21 00:47:20 +0800

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,56 @@
+import path from "path";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import rollupTypescript from "rollup-plugin-typescript2";
+import babel from "@rollup/plugin-babel";
+import { DEFAULT_EXTENSIONS } from "@babel/core";
+import { terser } from "rollup-plugin-terser";
+
+import pkg from "./package.json";
+const env = process.env.NODE_ENV;
+const name = "raft-otel-web";
+const config = {
+  input: path.resolve(__dirname, "src/index.ts"),
+  output: [
+    // commonjs
+    {
+      file: pkg.main,
+      format: "cjs",
+    },
+    // es module
+    {
+      file: pkg.module,
+      format: "es",
+    },
+    // umd
+    {
+      name,
+      file: pkg.umd,
+      format: "umd",
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    rollupTypescript(),
+    babel({
+      babelHelpers: "runtime",
+      exclude: "node_modules/**",
+      extensions: [...DEFAULT_EXTENSIONS, ".ts"],
+    }),
+  ],
+};
+
+if (env === "production") {
+  config.plugins.push(
+    terser({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false,
+      },
+    })
+  );
+}
+export default config;
